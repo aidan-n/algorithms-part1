bede8962a87c366d6c61c3e5df82488ab908db52
Sualeh Fatehi <sualeh@hotmail.com>
2017-05-15 23:06:59 -0400

    Randomized queue

--- stacks-and-queues/src/main/java/RandomizedQueue.java
+++ stacks-and-queues/src/main/java/RandomizedQueue.java
@@ -1,37 +1,64 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item>
   implements Iterable<Item>
 {
 
+  private class RandomizedIterator
+    implements Iterator<Item>
+  {
+    private int i;
+    private final Item[] b;
+
+    @SuppressWarnings("unchecked")
+    public RandomizedIterator()
+    {
+      b = (Item[]) new Object[n];
+      for (int i = 0; i < a.length; i++)
+      {
+        b[i] = a[i];
+      }
+      StdRandom.shuffle(b);
+    }
+
+    @Override
+    public boolean hasNext()
+    {
+      return i < b.length - 1;
+    }
+
+    @Override
+    public Item next()
+    {
+      if (!hasNext())
+      {
+        throw new NoSuchElementException();
+      }
+      return b[i--];
+    }
+
+    @Override
+    public void remove()
+    {
+      throw new UnsupportedOperationException();
+    }
+  }
+
+  private Item[] a; // array of items
+
+  private int n; // number of elements in queue
+
   /**
    * construct an empty randomized queue
    */
+  @SuppressWarnings("unchecked")
   public RandomizedQueue()
   {
-  }
-
-  /**
-   * is the queue empty?
-   */
-  public boolean isEmpty()
-  {
-    return false;
-  }
-
-  /**
-   * return the number of items on the queue
-   */
-  public int size()
-  {
-    return 0;
-  }
-
-  /**
-   * add the item
-   */
-  public void enqueue(Item item)
-  {
+    a = (Item[]) new Object[2];
+    n = 0;
   }
 
   /**
@@ -39,7 +66,48 @@ public class RandomizedQueue<Item>
    */
   public Item dequeue()
   {
-    return null;
+    if (n == 0)
+    {
+      throw new NoSuchElementException();
+    }
+    final Item item = a[n - 1];
+    a[n - 1] = null;
+    n--;
+    return item;
+  }
+
+  /**
+   * add the item
+   */
+  public void enqueue(final Item item)
+  {
+    if (item == null)
+    {
+      throw new NullPointerException();
+    }
+    if (n == a.length)
+    {
+      resize(2 * n);
+    }
+    a[n] = item;
+    n++;
+  }
+
+  /**
+   * is the queue empty?
+   */
+  public boolean isEmpty()
+  {
+    return n == 0;
+  }
+
+  /**
+   * return an independent iterator over items in random order
+   */
+  @Override
+  public Iterator<Item> iterator()
+  {
+    return new RandomizedIterator();
   }
 
   /**
@@ -47,22 +115,42 @@ public class RandomizedQueue<Item>
    */
   public Item sample()
   {
-    return null;
+    return a[randomInSize() - 1];
   }
 
   /**
-   * return an independent iterator over items in random order
+   * return the number of items on the queue
    */
-  public Iterator<Item> iterator()
+  public int size()
   {
-    return null;
+    return n;
   }
 
-  /**
-   * unit testing (optional)
-   */
-  public static void main(String[] args)
+  private int randomInSize()
   {
+    final int min = 1;
+    final int max = n;
+    return min + (int) (StdRandom.uniform() * (max - min + 1));
+  }
+
+  @SuppressWarnings("unchecked")
+  private void resize(final int capacity)
+  {
+    if (capacity <= n)
+    {
+      return;
+    }
+
+    // textbook implementation
+    final Item[] temp = (Item[]) new Object[capacity];
+    for (int i = 0; i < n; i++)
+    {
+      temp[i] = a[i];
+    }
+    a = temp;
+
+    // alternative implementation
+    // a = java.util.Arrays.copyOf(a, capacity);
   }
 
 }
