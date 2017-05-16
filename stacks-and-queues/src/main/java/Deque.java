bf32fb2f39238a589339e5c6b29cab7033551e85
Sualeh Fatehi <sualeh@hotmail.com>
2017-05-15 22:15:14 -0400

    Completed deque

--- stacks-and-queues/src/main/java/Deque.java
+++ stacks-and-queues/src/main/java/Deque.java
@@ -1,14 +1,95 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Deque<Item>
   implements Iterable<Item>
 {
+
+  private class Node
+  {
+    private Node next;
+    private Node previous;
+    private Item item;
+  }
+
+  /**
+   * unit testing (optional)
+   */
+  public static void main(final String[] args)
+  {
+  }
+
+  private int n;
+  private Node first;
+
+  private Node last;
 
   /**
    * construct an empty deque
    */
   public Deque()
   {
+    first = null;
+    last = null;
+    n = 0;
+    check();
+  }
+
+  /**
+   * add the item to the front
+   */
+  public void addFirst(final Item item)
+  {
+    if (item == null)
+    {
+      throw new NullPointerException();
+    }
+
+    final Node oldfirst = first;
+
+    first = new Node();
+    first.item = item;
+    first.next = oldfirst;
+    first.previous = null;
+
+    oldfirst.previous = first;
+
+    n++;
+
+    if (n == 1)
+    {
+      last = first;
+    }
+
+    check();
+  }
+
+  /**
+   * add the item to the end
+   */
+  public void addLast(final Item item)
+  {
+    if (item == null)
+    {
+      throw new NullPointerException();
+    }
+
+    final Node newLast = new Node();
+    newLast.item = item;
+    newLast.next = null;
+    newLast.previous = last;
+
+    last.next = newLast;
+    last = newLast;
+
+    n++;
+
+    if (n == 1)
+    {
+      first = last;
+    }
+
+    check();
   }
 
   /**
@@ -16,29 +97,16 @@ public class Deque<Item>
    */
   public boolean isEmpty()
   {
-    return false;
+    return first == null;
   }
 
   /**
-   * return the number of items on the deque
+   * return an iterator over items in order from front to end
    */
-  public int size()
+  @Override
+  public Iterator<Item> iterator()
   {
-    return 0;
-  }
-
-  /**
-   * add the item to the front
-   */
-  public void addFirst(Item item)
-  {
-  }
-
-  /**
-   * add the item to the end
-   */
-  public void addLast(Item item)
-  {
+    return null;
   }
 
   /**
@@ -46,7 +114,26 @@ public class Deque<Item>
    */
   public Item removeFirst()
   {
-    return null;
+    if (isEmpty())
+    {
+      throw new NoSuchElementException();
+    }
+
+    final Item item = first.item; // save item to return
+
+    first = first.next; // delete first node
+    first.previous = null;
+
+    n--;
+
+    if (n == 0)
+    {
+      last = null;
+    }
+
+    check();
+
+    return item; // return the saved item
   }
 
   /**
@@ -54,21 +141,84 @@ public class Deque<Item>
    */
   public Item removeLast()
   {
-    return null;
+    if (isEmpty())
+    {
+      throw new NoSuchElementException();
+    }
+
+    final Item item = last.item; // save item to return
+
+    last = last.previous; // delete last node
+
+    n--;
+
+    if (n == 0)
+    {
+      first = null;
+    }
+
+    check();
+
+    return item; // return the saved item
   }
 
   /**
-   * return an iterator over items in order from front to end
+   * return the number of items on the deque
    */
-  public Iterator<Item> iterator()
+  public int size()
   {
-    return null;
+    return n;
   }
 
-  /**
-   * unit testing (optional)
-   */
-  public static void main(String[] args)
+  private void check()
   {
+
+    // check a few properties of instance variable 'first'
+    if (n < 0)
+    {
+      throw new IllegalStateException("Bad deque size, " + n);
+    }
+    if (n == 0)
+    {
+      if (first != null)
+      {
+        throw new IllegalStateException("Inconsistent deque state");
+      }
+    }
+    else if (n == 1)
+    {
+      if (first == null)
+      {
+        throw new IllegalStateException("Inconsistent deque state");
+      }
+      if (first.next != null)
+      {
+        throw new IllegalStateException("Inconsistent deque state");
+      }
+    }
+    else
+    {
+      if (first == null)
+      {
+        throw new IllegalStateException("Inconsistent deque state");
+      }
+      if (first.next == null)
+      {
+        throw new IllegalStateException("Inconsistent deque state");
+      }
+    }
+
+    // check internal consistency of instance variable n
+    int numberOfNodes = 0;
+    for (Node x = first; x != null && numberOfNodes <= n; x = x.next)
+    {
+      numberOfNodes++;
+    }
+    if (numberOfNodes != n)
+    {
+      throw new IllegalStateException("Inconsistent deque state");
+    }
+
   }
+
 }
