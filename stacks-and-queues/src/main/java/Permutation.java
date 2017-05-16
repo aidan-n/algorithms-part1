aadd89499a304770f952669e98594c14bdadae8d
Sualeh Fatehi <sualeh@hotmail.com>
2017-05-15 23:58:35 -0400

    Permutation

--- stacks-and-queues/src/main/java/Permutation.java
+++ stacks-and-queues/src/main/java/Permutation.java
@@ -4,21 +4,32 @@ import edu.princeton.cs.algs4.StdOut;
 public class Permutation
 {
 
-  public static void main(String[] args)
+  public static void main(final String[] args)
   {
-    RandomizedQueue<String> queue = new RandomizedQueue<>();
 
-    final int k = Integer.parseInt(args[0]);
-    for (int i = 0; i < k; i++)
+    int k = Integer.parseInt(args[0]);
+    if (k <= 0)
     {
-      String string = StdIn.readString();
+      throw new IllegalArgumentException();
+    }
+
+    final RandomizedQueue<String> queue = new RandomizedQueue<>();
+    while (!StdIn.isEmpty())
+    {
+      final String string = StdIn.readString();
       queue.enqueue(string);
     }
 
-    for (String string: queue)
+    for (final String string: queue)
     {
       StdOut.println(string);
+      k--;
+      if (k == 0)
+      {
+        break;
+      }
     }
+
   }
 
 }
