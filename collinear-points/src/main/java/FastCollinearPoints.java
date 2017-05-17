416f42c4575b9d08acdc3b75b9577a00ba88c8f0
Sualeh Fatehi <sualeh@hotmail.com>
2017-05-16 21:40:17 -0400

    Initial commit for Week 3

---
+++ collinear-points/src/main/java/FastCollinearPoints.java
@@ -0,0 +1,28 @@
+
+public class FastCollinearPoints
+{
+
+  /**
+   * finds all line segments containing 4 points
+   */
+  public FastCollinearPoints(final Point[] points)
+  {
+  }
+
+  /**
+   * the number of line segments
+   */
+  public int numberOfSegments()
+  {
+    return 0;
+  }
+
+  /**
+   * the line segments
+   */
+  public LineSegment[] segments()
+  {
+    return null;
+  }
+
+}
